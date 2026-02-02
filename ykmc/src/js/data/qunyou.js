27acdfa82ffc37b49a512b347f5244f6e0cbfa38
lingfeng23333 <1507383042@qq.com>
2026-02-02 16:46:58 +0800

    Site updated: 2026-02-02 16:46:58

---
+++ ykmc/src/js/data/qunyou.js
@@ -0,0 +1,56 @@
+dataSetVersion = "2026-02-02"; // Custom version
+dataSet[dataSetVersion] = {};
+
+dataSet[dataSetVersion].options = [
+    {
+        name: "筛选群友 (Filter)",
+        key: "role",
+        tooltip: "选择要参与排序的群友分组",
+        checked: false,
+        sub: [
+            { name: "全部群友", key: "all" }
+        ]
+    }
+];
+
+dataSet[dataSetVersion].characterData = [
+    { name: "逸空", img: "逸空.png", opts: { role: ["all"] } },
+    { name: "幻梦", img: "幻梦.png", opts: { role: ["all"] } },
+    { name: "希尔", img: "希尔.png", opts: { role: ["all"] } },
+    { name: "北巡", img: "北巡.png", opts: { role: ["all"] } },
+    { name: "飞巡", img: "飞巡.png", opts: { role: ["all"] } },
+    { name: "佩恩", img: "佩恩.png", opts: { role: ["all"] } },
+    { name: "STA.HY", img: "STA.HY.png", opts: { role: ["all"] } },
+    { name: "伊凡", img: "伊凡.png", opts: { role: ["all"] } },
+    { name: "艾墨", img: "艾墨.png", opts: { role: ["all"] } },
+    { name: "檬夜", img: "檬夜.png", opts: { role: ["all"] } },
+    { name: "lxwys", img: "lxwys.png", opts: { role: ["all"] } },
+    { name: "墙砖", img: "墙砖.png", opts: { role: ["all"] } },
+    { name: "柏冉", img: "柏冉.png", opts: { role: ["all"] } },
+    { name: "塞法", img: "塞法.png", opts: { role: ["all"] } },
+    { name: "老二七", img: "老二七.png", opts: { role: ["all"] } },
+    { name: "阿瓦隆", img: "阿瓦隆.png", opts: { role: ["all"] } },
+    { name: "流云", img: "流云.png", opts: { role: ["all"] } },
+    { name: "Ra", img: "Ra.png", opts: { role: ["all"] } },
+    { name: "鹰目", img: "鹰目.png", opts: { role: ["all"] } },
+    { name: "Hierarch", img: "Hierarch.png", opts: { role: ["all"] } },
+    { name: "蓝蜻蜓", img: "蓝蜻蜓.png", opts: { role: ["all"] } },
+    { name: "妈死创", img: "妈死创.png", opts: { role: ["all"] } },
+    { name: "法国球", img: "法国球.png", opts: { role: ["all"] } },
+    { name: "1437", img: "1437.png", opts: { role: ["all"] } },
+    { name: "早八", img: "早八.png", opts: { role: ["all"] } },
+    { name: "Antiss", img: "Antiss.png", opts: { role: ["all"] } },
+    { name: "透镜", img: "透镜.png", opts: { role: ["all"] } },
+    { name: "一本", img: "一本.png", opts: { role: ["all"] } },
+    { name: "卡密哒", img: "卡密哒.png", opts: { role: ["all"] } },
+    { name: "Liberty", img: "Liberty.png", opts: { role: ["all"] } },
+    { name: "D1f", img: "D1f.png", opts: { role: ["all"] } },
+    { name: "凌风", img: "凌风.png", opts: { role: ["all"] } },
+    { name: "依雅莱", img: "依雅莱.png", opts: { role: ["all"] } },
+    { name: "Zjr", img: "Zjr.png", opts: { role: ["all"] } },
+    { name: "霜心台", img: "霜心台.png", opts: { role: ["all"] } },
+    { name: "Yuuki", img: "Yuuki.png", opts: { role: ["all"] } },
+    { name: "已矣哉", img: "已矣哉.png", opts: { role: ["all"] } },
+    { name: "鱼鱼", img: "鱼鱼.png", opts: { role: ["all"] } },
+    { name: "网课小学生", img: "网课小学生.png", opts: { role: ["all"] } }
+];
